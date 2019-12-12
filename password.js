9b8edb4b615a16206deefeaaa0a5ddadf62ef1a9
Carolene Wilson <carolenesw@gmail.com>
2019-12-11 23:19:57 -0500

    add variables for user prompts and alert with hints for password critera

--- password.js
+++ password.js
@@ -1,4 +1,4 @@
-//Add Variables to generate password 
+//Add arrays to generate password characters 
 
 var arrUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var arrlo = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -8,10 +8,16 @@ var arrNu = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var passW = "upperCase + lowerCase + numbers + symbols";
 // var lenght = 8 > 128;
 
+//Add variables to prompted user to select password criteria
 
-// Prompt user create a password 
+var upperCase = "Would you like your password to contain uppercase?";
+var lowerCase = "Would you like your password to contain lowercase?";
+var numbers = "Would you like your password to contain numbers?";
+var speChar = "Would you like your password to contain special characters?";
 
-var userPassWord = alert("Sign in with your unique Password, which MUST be consist of numbers, upper case, lower case and symbols");
+// Alert informing user of password requirements 
+
+var userHints = alert("Unique User-password MUST be consist of TWO of the following: numbers, upper case, lower case or symbols");
 
 //       // Converts the user's answer to lowercase.
 
