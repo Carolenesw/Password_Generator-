00ac0c6843d53628b87a7436f9b0bd8b37ccd0bd
Carolene Wilson <carolenesw@gmail.com>
2019-12-11 21:33:32 -0500

    add variables to js page

---
+++ password.js
@@ -0,0 +1,6 @@
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = 0123456789;
+var symbols = ~!'"@#$%^&*()_+`=-;':"[/]{?}|\";,.;
+
+var passW = "upperCase + lowerCase + numbers + symbols";
