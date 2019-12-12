f4bbd15a6f291f7daebd03d912d1f5badb247521
Carolene Wilson <carolenesw@gmail.com>
2019-12-11 23:07:19 -0500

    change var to arrays

--- password.js
+++ password.js
@@ -1,6 +1,23 @@
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = 0123456789;
-var symbols = ~!'"@#$%^&*()_+`=-;':"[/]{?}|\";,.;
+//Add Variables to generate password 
+
+var arrUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var arrlo = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var arrNu = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var arrSy = ["~", "!", ', @, #, $, %, ^, &, ", (), _, +, `, =, -, ;, :, ", [, ,, /, ], {, ?, }, |, \, ;, "(",")", "."];
 
 var passW = "upperCase + lowerCase + numbers + symbols";
+// var lenght = 8 > 128;
+
+
+// Prompt user create a password 
+
+var userPassWord = alert("Sign in with your unique Password, which MUST be consist of numbers, upper case, lower case and symbols");
+
+//       // Converts the user's answer to lowercase.
+
+//       var favoriteBandUserLower = favoriteBandUser.toLowerCase();
+
+//  // Converts the user's answer to lowercase.
+
+//  var favoriteBandUserLower = favoriteBandUser.toLowerCase();
+
