d9caee80abbd6fe6d291731aae78f1ffbb7d545d
Carolene Wilson <carolenesw@gmail.com>
2019-12-23 18:30:12 -0500

    function for random character using math random

--- password.js
+++ password.js
@@ -5,58 +5,26 @@ var arrLo = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var arrNu = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var arrSy = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "<", ".", ">", "/", "?", "'", ";", ":", "[", "{", "]", "}", "|", "]", "]", ";", "\""]
 
-// for (var i = 0; i < 128; i++) {
-//   var userPass = passW[Math.floor(Math.random() * passW.length)];
 
-
-// parseInt - return a string to a number 
-
-var userHints = alert("Unique User-password MUST be consist of minimum of One of the following: numbers, upper case, lower case or symbols");
-
-var userLenght = alert("Password lenght must be between 8 and 128 characters");
-
- 
-//confirm user inputs based on character selection
-
-var confirmUpperCase = confirm("Enter password with uppercase letters");
-var confirmLowerCase = confirm("Enter password with lowercase letters");
-var confirmNum = confirm("Enter password with numbers");
-var confirmSpeChar = confirm("Enter password with special characters");
-var confirmIn = ["confirmUpperCase", "confirmLowerCase", "confirmNum", "confirmSpeChar"];
-
-// create a function to select user password 
-
-
-// function passW (userPass) {
-
-const getPassW = (userPass) => {
-if (userPass === "arrUp" || userPass === "arrLo" || userPass === "arrNu" || userPass === "arrSy") {
- return userPass
-} else {
-  alert ("Error, please use required characters");
-}
+function charRandom(arr){
+  var temp = Math.floor(Math.random()*arr.length);
+  return arr[temp];
 }
 
-//   // alert ("enter");
-// } else if (userPass === "arrUp" && userPass === "arrLo") {
-//   alert ("enter");
-// } else if (userPass === "arrNu" && userPass === "arrSy") {
-//   alert ("enter");
-// } else if (userPass === "arrUp" && userPass === "arrNu") {
-//  alert ("enter");
-// } else if (userPass === "arrUp" && userPass === "arrSy") {
-//   alert ("enter");
-// } else if (userPass === "arrLo" && userPass === "arrSy") {
-//   alert ("enter");
-// } else if (userPass === "arrUp" && userPass === "arrLo" && userPass === "arrNu") {
-//   alert ("enter");
-// } else if (userPass === "arrLo" && userPass === "arrNu" && userPass === "arrSy") {
-//   alert ("enter");
-// } else if (userPass === "arrUp" && userPass === "arrLo" && userPass === "arrNu" && userPass === "arrSy") {
-//   alert ("enter");
-// } else {
-//   alert ("Error! Try again");
-// }
-// };
+function pwd(){
 
-// passW(userPass);
+  var pwdLength = parseInt(prompt("How many characters you want for your password?"));
+
+var confirmUpperCase = confirm("Do you want uppercase letters?");
+var confirmLowerCase = confirm("Do you want lowercase letters?");
+var confirmNum = confirm("Do you want numbers?");
+var confirmSpeChar = confirm("Do you want special characters?");
+var confirmIn = [confirmUpperCase, confirmLowerCase, confirmNum, confirmSpeChar];
+  var result = [];
+  var tempChar = [];
+  var finalChar = [];
+
+  
+
+
+//pwd();
